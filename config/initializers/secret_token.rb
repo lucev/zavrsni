1f3eef9fc8c662587748b633a24a390055bef84c
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-17 19:01:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tigris::Application.config.secret_token = '7fc5ede13286f2167266aab4f1cd11a72f6a58d3e2df563bcf3f8276e9f97023f95058c572890212c04adbcb5616c8b80e512045532b66966f47699bbeef9cdc'
