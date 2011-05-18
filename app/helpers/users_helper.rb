da96e58b0ca63ddf2380e6067352a3e1a16bcb22
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-18 05:07:57 +0200

    Added user types

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def options_for_user_types
+    options_for_select [
+      (:member),
+      (:user)
+    ]
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110517205348) do
+ActiveRecord::Schema.define(:version => 20110518020437) do
 
   create_table "organizations", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20110517205348) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "organization_id"
+    t.string   "type"
   end
 
 end
