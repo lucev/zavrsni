7ad8a57ee19802c2a011d03e6c8582e5fef17e2e
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-18 19:25:44 +0200

    Added simple style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
   def index
-    @users = User.all
+    @users = User.find(:all, :order => "surname, name ASC")
 
     respond_to do |format|
       format.html # index.html.erb
