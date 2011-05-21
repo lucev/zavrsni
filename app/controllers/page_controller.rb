b6da3e8f4f1df22f42965cd6bdda7bbb333e4ed3
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-21 18:05:10 +0200

    Added events on home page

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,6 +2,7 @@ class PageController < ApplicationController
 
   def home
     @news = News.find(:all, :order => "created_at DESC" )
+    @events = Event.find(:all, :order => "start_date ASC")
   end
 
 end
