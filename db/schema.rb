8e9913c267ad28aec7f06575ca2498ed82ff67f2
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-21 04:08:44 +0200

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110518154915) do
+ActiveRecord::Schema.define(:version => 20110521012556) do
 
   create_table "news", :force => true do |t|
     t.string   "title"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(:version => 20110518154915) do
     t.datetime "updated_at"
     t.integer  "organization_id"
     t.string   "type"
+    t.string   "password"
+    t.string   "password_salt"
   end
 
 end
