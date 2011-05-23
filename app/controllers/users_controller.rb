8c39828538185ae8b1fa01f7af00176eb988ace1
Ivan Lucev <lucev.ivan@gmail.com>
2011-05-23 12:52:07 +0200

    Always create Member

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
   # POST /users.xml
   def create
     model = params[:user][:type].camelize.constantize
-    @user = model.new(params[:user])
+    @user = Member.new(params[:user])
 
     respond_to do |format|
       if @user.save
