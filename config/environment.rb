8b931c04c0d968996dd33403c581b47c14fd4d24
Ivan Lucev <lucev.ivan@gmail.com>
2011-06-02 10:32:58 +0200

    Included styles

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ require File.expand_path('../application', __FILE__)
 Mime::Type.register "text/calendar", :ical
 Paperclip.options[:command_path] = "/usr/bin/"
 Paperclip::Railtie.insert
-Sass::Plugin.options[:template_location] = { 'app/styles' => 'tmp/stylesheets' }
 # Initialize the rails application
 Tigris::Application.initialize!
 
