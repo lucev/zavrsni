47ac94fd400b25f82be8b51edf1490633e910f89
Ivan Lucev <lucev.ivan@gmail.com>
2011-06-09 12:22:40 +0200

    Translated statuses on Croatian

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,18 @@
 module NewsHelper
+
+  def translate status
+    if status == 'public'
+      return 'javno'
+    elsif status == 'suggested'
+      return 'predloženo'
+    elsif status == 'internal'
+      return 'interno'
+    elsif status == 'hidden'
+      return 'skriveno'
+    else
+      return status
+    end
+  end
+
 end
+
